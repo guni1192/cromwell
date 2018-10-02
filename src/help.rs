8d8b076c4b4b7206894db34cb0c18bbf49ab1c14
guni973 <ad2314ce71926@gmail.com>
2018-10-02 16:46:26 +1000

    add help message

--- src/help.rs
+++ src/help.rs
@@ -1,3 +1,8 @@
 pub fn print_help() {
-    println!("help message");
+    println!("Usage ArchLinux Container Engine(ACE)");
+    println!("  --h --help \t print help message");
+    println!("      --name \t container name");
+    println!("      --exec \t container run command (default /bin/bash)");
+    println!("      --init \t bootstrap container os");
+    println!("      --del  \t delete container");
 }
