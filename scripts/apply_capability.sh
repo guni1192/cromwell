3ece8fdbc0b6a4892393a8d3f42fbe5935677fef
guni1192 <ad2314ce71926@gmail.com>
2019-02-03 20:13:16 +0000

    add circleci config, and remove travis.yml, scripts/

--- scripts/apply_capability.sh
+++
@@ -1,2 +0,0 @@
-setcap CAP_SYS_ADMIN=eip ./target/debug/cromwell
-getcap ./target/debug/cromwell
--- scripts/release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp ./target/release/ace /usr/local/bin/ace && setcap CAP_SYS_ADMIN=eip /usr/local/bin/ace
